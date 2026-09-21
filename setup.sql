ab86529b5567555ba96ce3599253bba9e1dc1680
Samuel Santini <samuel.santini.fernandes@gmail.com>
2026-09-21 11:19:16 -0300

    ajustando arquivos

--- setup.sql
+++ setup.sql
@@ -1,8 +1,8 @@
 -- Rode este arquivo no pgAdmin (Query Tool)
 -- antes de iniciar o servidor.
 --
--- 1) Crie o banco "fetch" pelo pgAdmin (botão direito em Databases > Create)
--- 2) Conecte no banco fetch e rode o CREATE TABLE abaixo
+-- 1) Crie o seu banco pelo pgAdmin (botão direito em Databases > Create)
+-- 2) Conecte no banco e rode o CREATE TABLE abaixo
 
 CREATE TABLE IF NOT EXISTS alunos (
   id SERIAL PRIMARY KEY,
